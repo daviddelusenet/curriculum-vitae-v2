152becba1a23bdd6fb30e7bdf80025350b3d0ef9
David de Lusenet <me@daviddeluse.net>
2023-07-24 11:29:53 +0200

    chore(dependencies): update multiple dependencies and fix linting errors

--- src/styles/mixins/underline.ts
+++ src/styles/mixins/underline.ts
@@ -1,20 +1,29 @@
 // This mixin is based on https://eager.io/blog/smarter-link-underlines/
-import { css, FlattenSimpleInterpolation } from "styled-components";
+import { css } from "styled-components";
 
-export const textShadowToCropUnderline = (
-  backgroundColor: string
-): FlattenSimpleInterpolation => css`
-  text-shadow: 0 0.03em ${backgroundColor}, 0 -0.03em ${backgroundColor},
-    0.03em 0 ${backgroundColor}, -0.03em 0 ${backgroundColor},
-    0.06em 0 ${backgroundColor}, -0.06em 0 ${backgroundColor},
-    0.09em 0 ${backgroundColor}, -0.09em 0 ${backgroundColor},
-    0.12em 0 ${backgroundColor}, -0.12em 0 ${backgroundColor},
-    0.15em 0 ${backgroundColor}, -0.15em 0 ${backgroundColor},
-    0.18em 0 ${backgroundColor}, -0.18em 0 ${backgroundColor};
+export const textShadowToCropUnderline = (backgroundColor: string) => css`
+  text-shadow:
+    0 0.03em ${backgroundColor},
+    0 -0.03em ${backgroundColor},
+    0.03em 0 ${backgroundColor},
+    -0.03em 0 ${backgroundColor},
+    0.06em 0 ${backgroundColor},
+    -0.06em 0 ${backgroundColor},
+    0.09em 0 ${backgroundColor},
+    -0.09em 0 ${backgroundColor},
+    0.12em 0 ${backgroundColor},
+    -0.12em 0 ${backgroundColor},
+    0.15em 0 ${backgroundColor},
+    -0.15em 0 ${backgroundColor},
+    0.18em 0 ${backgroundColor},
+    -0.18em 0 ${backgroundColor};
 `;
 
-export const animateUnderline = (): FlattenSimpleInterpolation => css`
-  background-size: 0.05em 1px, 0.05em 1px, 100% 1px;
+export const animateUnderline = () => css`
+  background-size:
+    0.05em 1px,
+    0.05em 1px,
+    100% 1px;
 `;
 
 export const underline = ({
@@ -27,20 +36,29 @@ export const underline = ({
   backgroundColor: string;
   color: string;
   underlinePosition?: string;
-}): FlattenSimpleInterpolation => css`
+}) => css`
   ${textShadowToCropUnderline(backgroundColor)}
   background-image: linear-gradient(${backgroundColor}, ${backgroundColor}),
         linear-gradient(${backgroundColor}, ${backgroundColor}),
         linear-gradient(${color}, ${color});
-  background-position: 0 90%, 100% 90%, 0 ${underlinePosition};
+  background-position:
+    0 90%,
+    100% 90%,
+    0 ${underlinePosition};
   background-repeat: no-repeat, no-repeat, no-repeat;
 
   ${animate
     ? css`
         transition: background-size 0.7s cubic-bezier(0.25, 0.46, 0.45, 0.94);
-        background-size: 0.05em 1px, 0.05em 1px, 0 1px;
+        background-size:
+          0.05em 1px,
+          0.05em 1px,
+          0 1px;
       `
     : css`
-        background-size: 0.05em 1px, 0.05em 1px, 100% 1px;
+        background-size:
+          0.05em 1px,
+          0.05em 1px,
+          100% 1px;
       `}
 `;
